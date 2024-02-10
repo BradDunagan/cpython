115aa3c84480845cb22e712669ee84fa87111e97
Brad Duangan <brad.dunagan@gmail.com>
2024-02-10 14:18:50 -0800

    rr_ui module - adding canvas functions

--- Modules/rr_ui.c
+++ Modules/rr_ui.c
@@ -35,8 +35,8 @@ Notes on -
 	this is out of scope for this document and can be found in the Python 
 	Packaging User’s Guide."
 
-	To add parameters to the ui.CanvasContext() call (when creating a CanvasContext
-	object) see -
+	To add parameters to the ui.CanvasContext() call (when creating a 
+	CanvasContext object) see -
 
 		https://docs.python.org/3.7/extending/newtypes_tutorial.html#adding-data-and-methods-to-the-basic-example
 
@@ -77,10 +77,11 @@ static PyObject *		UICBResult = NULL;
 typedef struct {
 	PyObject_HEAD
 
-	int			frameId;
-	int			paneId;
-	PyObject *	panelName;		//	e.g., "pnlMain"
-	PyObject *	canvasName;		//	e.g., "cvsA"
+//	int			frameId;
+//	int			paneId;
+	PyObject *	pane;			//	e.g., "Controls"
+	PyObject *	panel;			//	e.g., "pnlMain"
+	PyObject *	canvas;			//	e.g., "cvsA"
 	int			hContext;		//	i.e., context handle
 
 }	CanvasContext;
@@ -88,8 +89,9 @@ typedef struct {
 static void
 CanvasContext_dealloc ( CanvasContext * self )
 {
-	Py_XDECREF(self->panelName);
-	Py_XDECREF(self->canvasName);
+	Py_XDECREF(self->pane);
+	Py_XDECREF(self->panel);
+	Py_XDECREF(self->canvas);
 	Py_TYPE(self)->tp_free ( (PyObject *) self );
 }
 
@@ -101,14 +103,18 @@ CanvasContext_new ( PyTypeObject * type, PyObject * args, PyObject * kwds )
 	if ( self == NULL ) {
 		return NULL; }
 
-	self->frameId  = 0;
-	self->paneId   = 0;
-	self->panelName = PyUnicode_FromString ( "" );
-	if ( self->panelName == NULL ) {
+//	self->frameId  = 0;
+//	self->paneId   = 0;
+	self->pane = PyUnicode_FromString ( "" );
+	if ( self->pane == NULL ) {
 		Py_DECREF(self);
 		return NULL; }
-	self->canvasName = PyUnicode_FromString ( "" );
-	if (self->canvasName == NULL) {
+	self->panel = PyUnicode_FromString ( "" );
+	if ( self->panel == NULL ) {
+		Py_DECREF(self);
+		return NULL; }
+	self->canvas = PyUnicode_FromString ( "" );
+	if (self->canvas == NULL) {
 		Py_DECREF(self);
 		return NULL; }
 	self->hContext = 0;
@@ -126,25 +132,31 @@ CanvasContext_init ( CanvasContext * self, PyObject * args, PyObject * kwds )
 		PyErr_SetString ( UIError, "Callback in progress, but no canvas context." );
 		return -1; }
 
-	static char *	kwlist[] = { "frameId", "paneId", "panelName", "canvasName", 
+//	static char *	kwlist[] = { "frameId", "paneId", "panelName", "canvasName", 
+	static char *	kwlist[] = { "pane", "panel", "canvas", 
 								 NULL };
-	PyObject * panelName = NULL, * canvasName = NULL, * tmp;
+	PyObject * pane = NULL, * panel = NULL, * canvas = NULL, * tmp;
 
-	if ( ! PyArg_ParseTupleAndKeywords ( args, kwds, "iiUU", kwlist,
-										 &self->frameId, &self->paneId,
-										 &panelName, &canvasName ) ) {
+	if ( ! PyArg_ParseTupleAndKeywords ( args, kwds, "UUU", kwlist,
+										 &pane, &panel, &canvas) ) {
 		return -1; }
 
-	if ( panelName ) {
-		tmp = self->panelName;
-		Py_INCREF(panelName);
-		self->panelName = panelName;
+	if ( pane ) {
+		tmp = self->pane;
+		Py_INCREF(pane);
+		self->pane = pane;
 		Py_DECREF(tmp); }
 
-	if ( canvasName ) {
-		tmp = self->canvasName;
-		Py_INCREF(canvasName);
-		self->canvasName = canvasName;
+	if ( panel ) {
+		tmp = self->panel;
+		Py_INCREF(panel);
+		self->panel = panel;
+		Py_DECREF(tmp); }
+
+	if ( canvas ) {
+		tmp = self->canvas;
+		Py_INCREF(canvas);
+		self->canvas = canvas;
 		Py_DECREF(tmp); }
 
 	//	Callback to the app to get the context handle.
@@ -157,17 +169,22 @@ CanvasContext_init ( CanvasContext * self, PyObject * args, PyObject * kwds )
 	PyObject * uSelf = PyUnicode_FromFormat ( "%llu", 
 											  (unsigned long long)self );
 	PyObject * o;
+//	o = UICB ( BRADDS_F_FLAGS_UI_CALL,
+//	//	Do not specify BRADDS_F_FLAGS_UI_CALL because we don't need/want to 
+	//	execute the statement again.
+	//	But we do need to have the app provide a canvas context.  How?
+	//		-	Finish the statement, but, in the response to the app some
+	//			command or status to get and provide the canvas context before 
+	//			executing the next statement.
 	o = UICB ( 0,						//	don't execute the statement again
 			   "[ { \"cmd\": \"get-canvas-context\", "
 				  " \"context-object\": \"%s\", "
-				  " \"frameId\": %d, "
-				  " \"paneId\": %d, "
-				  " \"panel-name\": \"%s\", "
-				  " \"canvas-name\": \"%s\" } ]", PyUnicode_DATA ( uSelf ),
-										  self->frameId,
-										  self->paneId,
-										  PyUnicode_DATA ( self->panelName ),
-										  PyUnicode_DATA ( self->canvasName ) );
+				  " \"pane\": \"%s\", "
+				  " \"panel\": \"%s\", "
+				  " \"canvas\": \"%s\" } ]", PyUnicode_DATA ( uSelf ),
+										  PyUnicode_DATA ( self->pane),
+										  PyUnicode_DATA ( self->panel ),
+										  PyUnicode_DATA ( self->canvas ) );
 	Py_DECREF(uSelf);
 
 	if ( o == Py_None ) {
@@ -178,11 +195,82 @@ CanvasContext_init ( CanvasContext * self, PyObject * args, PyObject * kwds )
 }	//	CanvasContext_init()
 
 
-static PyObject *
-CanvasContext_fillstyle ( CanvasContext * self, PyObject * args )
+static void
+Error ( const char * fmt, ... )
 {
-	const char * style;
+	char buf [128];		buf[sizeof(buf) - 1] = '\0';
 
+	va_list	alist;	va_start ( alist, fmt );
+
+	vsnprintf ( buf, sizeof(buf) - 1, fmt, alist );
+
+	PyErr_SetString ( UIError, buf );
+
+}	//	Error()
+
+static PyObject *
+CtxCallPart2 ( const char * fnc, PyObject * response )
+{
+	if ( response == NULL ) {
+		Error ( "%s: no result", fnc );
+		return NULL; }
+
+	if ( ! PyList_Check ( response ) ) {
+		Error ( "%s: result is not a list", fnc );
+		return NULL; }
+
+	int n = PySequence_Length ( response );
+
+	if ( n != 1 ) {
+		Error ( "%s: expected 1 dict in result list", fnc ); 
+		return NULL; }
+
+	PyObject * d = PySequence_GetItem ( response, 0 );
+
+	if ( ! PyDict_Check ( d ) ) {
+		Error ( "%s: expected dict item in result list", fnc );
+		return NULL; }
+
+	//	From whence status, result?
+	//	-	UICB result, a PyObject *.
+	//	-	Gotten from the second UICB(), just above.
+	//	-	Which itself is gotten by/from utils.c:ParseValue().
+	//	-	It is the response from the app. I.e., one of the dicts
+	//		in the array of responses.
+	//	-	So, here, we use a PyDict_... API call to get the status
+	//		and result items.
+
+	PyObject *	statusO = PyDict_GetItemString ( d, "status" );
+	PyObject *	resultO = PyDict_GetItemString ( d, "result" );
+
+	if ( PyUnicode_KIND ( statusO ) != PyUnicode_1BYTE_KIND ) {
+		Error ( "%s: expected status in dict item", fnc );
+		return NULL; }
+
+	if ( ! resultO ) {
+		Error ( "%s: expect result object in dict item" );
+		return NULL; }
+
+	const char * status = PyUnicode_DATA ( statusO );
+
+	if ( strcmp ( status, "ok" )  == 0 ) {
+		//	result may be any PyObject *.
+		Py_INCREF(resultO);
+		return resultO; }
+
+	//	If status != "error" then result should always be a string (i.e., 
+	//	an error message).
+
+//	PyErr_SetObject ( UIError, result );
+	Error ( "%s: %s", fnc,  status );
+
+	return NULL; 
+
+}	//	CtxCallPart2()
+
+static PyObject *
+CanvasContext_fillStyle ( CanvasContext * self, PyObject * args )
+{
 	if ( UICBInProgress ) {
 		UICBInProgress = 0;
 
@@ -193,52 +281,57 @@ CanvasContext_fillstyle ( CanvasContext * self, PyObject * args )
 		//	Part 2.  Get the app response from the callback.
 		PyObject * o = UICB ( 0, NULL );
 
-		if ( o == NULL ) {
-			PyErr_SetString ( UIError, "fillstyle: no result" );
-			return NULL; }
+	//	if ( o == NULL ) {
+	//		PyErr_SetString ( UIError, "fillstyle: no result" );
+	//		return NULL; }
+	//
+	//	if ( ! PyList_Check ( o ) ) {
+	//		PyErr_SetString ( UIError, "fillstyle: result is not a list" );
+	//		return NULL; }
+	//
+	//	int n = PySequence_Length ( o );
+	//
+	//	if ( n != 1 ) {
+	//		PyErr_SetString ( UIError, "fillstyle: expected 1 dict in result "
+	//								   "list" );
+	//		return NULL; }
+	//
+	//	PyObject * d = PySequence_GetItem ( o, 0 );
+	//
+	//	if ( ! PyDict_Check ( d ) ) {
+	//		PyErr_SetString ( UIError, "fillstyle: expected dict item in result "
+	//								   "list" );
+	//		return NULL; }
+	//
+	//	//	From whence status, result?
+	//	//	-	UICB result, a PyObject *.
+	//	//	-	Gotten from the second UICB(), just above.
+	//	//	-	Which itself is gotten by/from utils.c:ParseValue().
+	//	//	-	It is the response from the app. I.e., one of the dicts
+	//	//		in the array of responses.
+	//	//	-	So, here, we use a PyDict_... API call to get the status
+	//	//		and result items.
+	//
+	//	PyObject *	status = PyDict_GetItemString ( d, "status" );
+	//	PyObject *	result = PyDict_GetItemString ( d, "result" );
+	//
+	//	if ( strcmp ( status, "ok" )  == 0 ) {
+	//		//	result may be any PyObject *.
+	//		Py_INCREF(result);
+	//		Py_DECREF(o);	o = NULL;
+	//		return result; }
+	//
+	//	//	If status != "error" then result should always be a string (i.e., 
+	//	//	an error message).
+	//
+	//	PyErr_SetObject ( UIError, result );
+	//	Py_DECREF(o);	o = NULL;
+	//	return NULL; }
+		PyObject * r = CtxCallPart2 ( "fillStyle", o ); 
 
-		if ( ! PyList_Check ( o ) ) {
-			PyErr_SetString ( UIError, "fillstyle: result is not a list" );
-			return NULL; }
-
-		int n = PySequence_Length ( o );
-
-		if ( n != 1 ) {
-			PyErr_SetString ( UIError, "fillstyle: expected 1 dict in result "
-									   "list" );
-			return NULL; }
-
-		PyObject * d = PySequence_GetItem ( o, 0 );
-
-		if ( ! PyDict_Check ( d ) ) {
-			PyErr_SetString ( UIError, "fillstyle: expected dict item in result "
-									   "list" );
-			return NULL; }
-
-		//	From whence status, result?
-		//	-	UICB result, a PyObject *.
-		//	-	Gotten from the second UICB(), just above.
-		//	-	Which itself is gotten by/from utils.c:ParseValue().
-		//	-	It is the response from the app. I.e., one of the dicts
-		//		in the array of responses.
-		//	-	So, here, we use a PyDict_... API call to get the status
-		//		and result items.
-
-		PyObject *	status = PyDict_GetItemString ( d, "status" );
-		PyObject *	result = PyDict_GetItemString ( d, "result" );
-
-		if ( strcmp ( status, "ok" )  == 0 ) {
-			//	result may be any PyObject *.
-			Py_INCREF(result);
-			Py_DECREF(o);	o = NULL;
-			return result; }
-
-		//	If status != "error" then result should always be a string (i.e., 
-		//	an error message).
-
-		PyErr_SetObject ( UIError, result );
 		Py_DECREF(o);	o = NULL;
-		return NULL; }
+
+		return r; }
 
 	//	Part 1.
 	//
@@ -252,36 +345,131 @@ CanvasContext_fillstyle ( CanvasContext * self, PyObject * args )
 	//		https://docs.python.org/3/c-api/arg.html?highlight=pyarg_parsetuple#parsing-arguments
 	//		https://docs.python.org/3/c-api/arg.html?highlight=pyarg_parsetuple#c.PyArg_ParseTuple
 	//
-	if ( ! PyArg_ParseTuple ( args, "s:fillstyle", &style ) ) {
+	const char * style;
+
+	if ( ! PyArg_ParseTuple ( args, "s:fillStyle", &style ) ) {
 		return NULL; }
 
 	PyObject * uSelf = PyUnicode_FromFormat ( "%llu", 
 											  (unsigned long long)self );
 	PyObject * o;
 	o = UICB ( BRADDS_F_FLAGS_UI_CALL,
-			   "[ { \"cmd\": \"set-canvas-fillstyle\", "
+			   "[ { \"cmd\": \"canvas-set-fillstyle\", "
 				  " \"context-object\": \"%s\", "
-				  " \"frameId\": %d, "
-				  " \"paneId\": %d, "
-				  " \"panel-name\": \"%s\", "
-				  " \"canvas-name\": \"%s\", "
+				  " \"pane\": \"%s\", "
+				  " \"panel\": \"%s\", "
+				  " \"canvas\": \"%s\", "
 				  " \"context-handle\": %d, "
 				  " \"fillstyle\": \"%s\"  } ]", PyUnicode_DATA ( uSelf ),
-										  self->frameId,
-										  self->paneId,
-										  PyUnicode_DATA ( self->panelName ),
-										  PyUnicode_DATA ( self->canvasName ),
+										  PyUnicode_DATA ( self->pane ),
+										  PyUnicode_DATA ( self->panel ),
+										  PyUnicode_DATA ( self->canvas ),
 										  self->hContext,
 										  style );
 	Py_DECREF(uSelf);
 
 	return o;
 
-}	//	CanvasContext_fillstyle()
+}	//	CanvasContext_fillStyle()
+
+static PyObject *
+CanvasContext_fillRect ( CanvasContext * self, PyObject * args )
+{
+	if ( UICBInProgress ) {
+		UICBInProgress = 0;
+
+		//	Part 2.  Get the app response from the callback.
+		//
+		PyObject * o = UICB ( 0, NULL );
+
+		PyObject * r = CtxCallPart2 ( "fillRect", o ); 
+
+		Py_DECREF(o);	o = NULL;
+
+		return r; }
+
+	//	Part 1.
+	//
+
+	int x, y, w, h;
+
+	if ( ! PyArg_ParseTuple ( args, "iiii:fillRect", &x, &y, &w, &h) ) {
+		return NULL; }
+
+	PyObject * uSelf = PyUnicode_FromFormat ( "%llu", 
+											  (unsigned long long)self );
+	PyObject * o;
+	o = UICB ( BRADDS_F_FLAGS_UI_CALL,
+			   "[ { \"cmd\": \"canvas-fill-rect\", "
+				  " \"context-object\": \"%s\", "
+				  " \"pane\": \"%s\", "
+				  " \"panel\": \"%s\", "
+				  " \"canvas\": \"%s\", "
+				  " \"context-handle\": %d, "
+				  " \"x\": %d, \"y\": %d, \"w\": %d, \"h\": %d  } ]", 
+				PyUnicode_DATA ( uSelf ),
+				PyUnicode_DATA ( self->pane ),
+				PyUnicode_DATA ( self->panel ),
+				PyUnicode_DATA ( self->canvas ),
+				self->hContext,
+				x, y, w, h );
+
+	Py_DECREF(uSelf);
+
+	return o;
+
+}	//	CanvasContext_fillRect()
+
+static PyObject *
+CanvasContext_translate ( CanvasContext * self, PyObject * args )
+{
+	Error ( "%s: implement this", "translate" );
+
+	//	Part 2.
+
+
+	//	Part 1.
+
+
+}	//	CanvasContext_translate()
+
+static PyObject *
+CanvasContext_rotate ( CanvasContext * self, PyObject * args )
+{
+	Error ( "%s: implement this", "rotate" );
+
+	//	Part 2.
+
+
+	//	Part 1.
+
+
+}	//	CanvasContext_rotate()
+
+static PyObject *
+CanvasContext_getImageData ( CanvasContext * self, PyObject * args )
+{
+	Error ( "%s: implement this", "getImageData" );
+
+	//	Part 2.
+
+
+	//	Part 1.
+
+
+}	//	CanvasContext_getImageData()
 
 static PyMethodDef CanvasContext_methods[] = {
-    { "fillstyle", (PyCFunction) CanvasContext_fillstyle, METH_VARARGS,
+    { "fillStyle",		(PyCFunction)CanvasContext_fillStyle,		METH_VARARGS,
       "Set the fill stiyle for a canvas." },
+    { "fillRect",		(PyCFunction)CanvasContext_fillRect,		METH_VARARGS,
+      "Fill a rectangle with the current fill style." },
+    { "translate",		(PyCFunction)CanvasContext_translate,		METH_VARARGS,
+      "Translate." },
+    { "rotate",			(PyCFunction)CanvasContext_rotate,			METH_VARARGS,
+      "Rotate." },
+    { "getImageData",	(PyCFunction)CanvasContext_getImageData,	METH_VARARGS,
+      "Get image data." },
     { NULL }  /* Sentinel */
 };
 
@@ -340,7 +528,7 @@ static PyObject *	setCallback ( PyObject * self, PyObject * args ) {
 PyDoc_STRVAR(setCanvasContextHandle_doc, 
 		"setCanvasContextHandle()\n\nSet/keep the handle to a canvas context.");
 
-static PyObject *	setCanvasContextHandle( PyObject * self, PyObject * args ) {
+static PyObject *	setCanvasContextHandle ( PyObject * self, PyObject * args ) {
 
 	const char * sCC;	int handle;
 	if ( ! PyArg_ParseTuple ( args, "si:setCanvasContextHandle", &sCC, &handle ) )
